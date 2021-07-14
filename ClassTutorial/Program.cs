59eb68a6b6ecca20663ccd9809ae6158247aea9a
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:32:55 -0400

    Committed by GitUtil

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassTutorial {
+    class Program {
+        static void Main(string[] args) {
+
+            var m1 = new Major("Math");
+            var m2 = new Major("Engineering");
+            var m3 = new Major("Education");
+            m3.Id = 88;
+
+
+            
+            //var tqlmath = new TqlMath(21, 30);
+            //tqlmath.A = 41;
+            ////tqlmath.A = 21;
+            ////tqlmath.B = 30;
+            //var sum = tqlmath.Sum();
+            //Console.WriteLine($"Sum of {tqlmath.A} and {tqlmath.B} is {sum}");
+            //var diff = tqlmath.Diff();
+            //Console.WriteLine($"Diff of {tqlmath.A} and {tqlmath.B} is {diff}");
+        }
+    }
+}
---
+++ ClassTutorial/TqlMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTutorial {
+    class TqlMath {
+
+        public TqlMath(int a) {
+            this.A = a;
+            this.B = a;
+        }
+        public TqlMath(int a, int B) {
+            A = a;
+            if(B <= 0) {
+                Console.WriteLine("B must be GT zero");
+                return;
+            }
+            this.B = B;
+        }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public int Sum() {
+            return A + B;
+        }
+        public int Diff() {
+            return A - B;
+        }
+    }
+}
